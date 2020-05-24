b15f9aa301da32ea7fc6c638da1ae79eeb45ab9a
tarasm2 <65740473+tarasm2@users.noreply.github.com>
2020-05-24 15:20:43 -0700

    Update StateMachine.sv

--- StateMachine.sv
+++ StateMachine.sv
@@ -6,14 +6,14 @@
 // and then depending on what that instruction is, the next state is decided, with the ability to execute multiple commands, labeled by the state names. It runs on the clock that the whole
 // processor uses and outputs all the data necessary for the computaions or exectution of the instruction.
 
-module StateMachine(reset, data, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_W_addr, RF_W_en, RF_Ra_addr, RF_Rb_addr, CurrentState, NextState);
-    input logic [15:0] data;                                // input to state machine
-    output logic PC_clr, PC_up, IR_ld, RF_s, RF_W_en, reset;// counter clear, count up, reg load, mux select, write, and reset enable signals
-    output logic [3:0] RF_W_addr, RF_Ra_addr, RF_Rb_addr;   // write, readA, and readB addresses for Regiester File
-    output logic [2:0] Alu_s0;                              // ALU select bits
-    output logic [7:0] D_addr;                              // address to be written to/read from in Data Memory
+module StateMachine(clk, reset, data, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_W_addr, RF_W_wr, RF_Ra_addr, RF_Rb_addr, ALU_s0, CurrentState, NextState);
+    input logic [15:0] data;                                              // input to state machine
+    output logic PC_clr, PC_up, IR_ld, RF_s, RF_W_wr, reset, D_wr, clk;   // counter clear, count up, reg load, mux select, write, reset enable, data write and clock signals
+    output logic [3:0] RF_W_addr, RF_Ra_addr, RF_Rb_addr;                 // write, readA, and readB addresses for Regiester File
+    output logic [2:0] Alu_s0;                                            // ALU select bits
+    output logic [7:0] D_addr;                                            // address to be written to/read from in Data Memory
 
-    output logic [3:0] CurrentState, NextState;    // declaring the two states in teh state machine
+    output logic [3:0] CurrentState, NextState;                     // declaring the two states in teh state machine
 
         localparam                          // declaring all the states within the state machine
             Init   = 0,
@@ -47,7 +47,7 @@ module StateMachine(reset, data, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_W_
             Decode:begin                        // depending on first four numbers of input, choose which operation is going to be done
                 if(data[15:12] == 0)      NextState = NOOP;          
                 else if(data[15:12] == 1) NextState = Store;
-                else if(data[15:12] == 2) NextState = Load;
+                else if(data[15:12] == 2) NextState = Load_A;
                 else if(data[15:12] == 3) NextState = Add;
                 else if(data[15:12] == 4) NextState = Sub;
                 else                      NextState = Halt;
@@ -55,31 +55,31 @@ module StateMachine(reset, data, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_W_
             NOOP:   NextState = Fetch;          // no operations in this state, go back to fetch
             Load_A: NextState = Load_B;         // the Load operation takes two clock cycles to toggle, this is a stopgap to take a clockcycle
             Load_B:begin
-                D_addr = IR[11:4];              // address being sent to datapath memory to be read/written
+                D_addr = data[11:4];              // address being sent to datapath memory to be read/written
                 RF_s = 1;                       // select bit for the mux chooses to load in something from the data Memory
-                RF_W_addr = IR[3:0];            // address in the Register file where the data will be written to
+                RF_W_addr = data[3:0];            // address in the Register file where the data will be written to
                 RF_W_en = 1;                    // enable the write command in the register file to allow writing to it
                 NextState = Fetch;              // Nextstate will go back to fetch next command
             end
             Store:begin
-                D_addr = IR[7:0];               // address being sent to datapath memory where the information will be stored within it
+                D_addr = data[7:0];               // address being sent to datapath memory where the information will be stored within it
                 D_wr = 1;                       // write enable bit for data memory
                 RF_Ra_addr=IR[11:8];            // data being saved from register to data memeory
                 NextState = Fetch;              // Nextstate will go back to fetch next command
             end
             Add:begin
-                RF_W_addr = IR[3:0];            // address that the sum will be written to within the register file
+                RF_W_addr = data[3:0];            // address that the sum will be written to within the register file
                 RF_W_wr = 1;                    // enable to write to the register file
-                RF_Ra_addr=IR[11:8];            // first number being added
-                RF_Rb_addr = IR[7:4];           // second number being added
+                RF_Ra_addr = data[11:8];            // first number being added
+                RF_Rb_addr = data[7:4];           // second number being added
                 ALU_s0 = 1;                     // tell the ALU to add the two numbers
                 NextState = Fetch;              // Nextstate will go back to fetch next command
             end
             Sub:begin
-                RF_W_addr = IR[3:0];            // address that the sum will be written to within the register file
+                RF_W_addr = data[3:0];            // address that the sum will be written to within the register file
                 RF_W_wr = 1;                    // enable to write to the register file
-                RF_Ra_addr=IR[11:8];            // number that will be subrtacted from
-                RF_Rb_addr = IR[7:4];           // number that will be subtracted from above number
+                RF_Ra_addr= data[11:8];            // number that will be subrtacted from
+                RF_Rb_addr = data[7:4];           // number that will be subtracted from above number
                 ALU_s0 = 2;                     // ALU signal to do the subtracting
                 NextState = Fetch;              // Nextstate will go back to fetch next command
             end
@@ -89,11 +89,11 @@ module StateMachine(reset, data, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_W_
 endmodule
 
 module StateMachine_tb(reset, data, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_W_addr, RF_W_en, RF_Ra_addr, RF_Rb_addr, CurrentState, NextState);
-    logic [15:0] data;                                   // input to state machine
-    logic PC_clr, PC_up, IR_ld, RF_s, RF_W_en, reset;    // counter clear, count up, reg load, mux select, write, and reset enable signals
-    logic [3:0] RF_W_addr, RF_Ra_addr, RF_Rb_addr;       // write, readA, and readB addresses for Regiester File
-    logic [2:0] Alu_s0;                                  // ALU select bits
-    logic [7:0] D_addr;                                  // address to be written to/read from in Data Memory
+    logic [15:0] data;                                             // input to state machine
+    logic PC_clr, PC_up, IR_ld, RF_s, RF_W_wr, reset, D_wr, clk;   // counter clear, count up, reg load, mux select, write, reset enable, data write and clock signals
+    logic [3:0] RF_W_addr, RF_Ra_addr, RF_Rb_addr;                 // write, readA, and readB addresses for Regiester File
+    logic [2:0] Alu_s0;                                            // ALU select bits
+    logic [7:0] D_addr;                                            // address to be written to/read from in Data Memory
     logic [3:0] CurrentState, NextState;
 
     StateMachine U1(reset, data, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_W_addr, RF_W_en, RF_Ra_addr, RF_Rb_addr, CurrentState, NextState);
