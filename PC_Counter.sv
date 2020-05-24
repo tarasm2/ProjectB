161c3e1bd026e6fca342e21787962b4293b396a8
tarasm2 <65740473+tarasm2@users.noreply.github.com>
2020-05-24 00:35:03 -0700

    Update PC_Counter.sv

--- PC_Counter.sv
+++ PC_Counter.sv
@@ -2,9 +2,11 @@
 // Taras Martynyuk, Ben Mulyarchuk, Kevin Nguyen
 // 5/20/2020
 // ProjectB
-// decription...
+// Program counter that increments as the next instruction is fetched. When an high input signal is given (by the state machine) to count up the 
+// counter should count up by one and sent that number (8 bit binary) as the output. This output is used to acces the next address location in the 
+// instruction memory 
 
-module PC_Counter(clk,up, clear, address);
+module PC_Counter(clk, up, clear, address);
     input logic up, clear, clk;       // increase counter, clock and clear signals
     output logic [6:0] address;       // output address
 
@@ -14,4 +16,25 @@ module PC_Counter(clk,up, clear, address);
         else address <= address;                    // else stay at same count
     end
 
+endmodule
+
+//testbench
+module PC_Counter_tb(clk, up, clear, address);
+    logic up, clear, clk;       // increase counter, clock and clear signals
+    logic [6:0] address;        // output address
+
+    PC_Counter U1(clk, up, clear, address);
+
+    always begin        // clock signal
+        clk = 0; #10;
+        clk = 1; #10;
+    end
+
+    initial begin
+        clear = 1; up = 0; #22;         // reset the counter
+        clear = 0; up = 1; #22;         // start counting up
+        wait(address = 0); #82;         // wait one full cycle and a bit longer to see it loop around
+        clear = 1;         #22;         // make sure counter resets
+        $stop;
+    end
 endmodule
